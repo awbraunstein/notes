8b33f2002d296be7f206c19b07b9c56248d1d363
Andrew Braunstein <awbraunstein@gmail.com>
2011-02-20 17:59:25 -0500

    Initial Commit

---
+++ notes/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  notes
+//
+//  Created by Andrew Braunstein on 2/20/11.
+//  Copyright 2011 UPenn. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
